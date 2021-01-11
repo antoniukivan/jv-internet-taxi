79ae8b93dc468544ed113d5b65ab5c489ab0d2e8
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-11 22:27:01 +0200

    add class ManufacturerDao to package dao

---
+++ src/main/java/mate/academy/dao/ManufacturerDao.java
@@ -0,0 +1,20 @@
+package mate.academy.dao;
+
+import mate.academy.model.Manufacturer;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ManufacturerDao {
+    public Manufacturer create(Manufacturer manufacturer) {
+
+    }
+
+    public Optional<Manufacturer> get(Long id) {
+
+    }
+
+    public List<Manufacturer> getAll() {
+
+    }
+}
