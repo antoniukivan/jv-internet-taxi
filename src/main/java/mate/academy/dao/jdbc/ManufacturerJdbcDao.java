363089e463dd34c27d5109ee37af5201a7df95cf
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-14 13:34:04 +0200

    refactor according to student review

--- src/main/java/mate/academy/dao/jdbc/ManufacturerJdbcDao.java
+++ src/main/java/mate/academy/dao/jdbc/ManufacturerJdbcDao.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import mate.academy.dao.ManufacturerDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
@@ -28,7 +29,7 @@ public class ManufacturerJdbcDao implements ManufacturerDao {
             preparedStatement.executeUpdate();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet != null && resultSet.next()) {
-                manufacturer.setId(resultSet.getLong(1));
+                manufacturer.setId(resultSet.getObject("manufacturer_id", Long.class));
             }
         } catch (SQLException e) {
             throw new DataProcessingException("Can't insert manufacturer to the DB "
@@ -42,7 +43,7 @@ public class ManufacturerJdbcDao implements ManufacturerDao {
         String query = "SELECT * FROM manufacturers WHERE (manufacturer_id = ?) "
                 + "AND (manufacturer_deleted = 'FALSE')";
         try (Connection connection = ConnectionUtil.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet != null && resultSet.next()) {
@@ -59,7 +60,7 @@ public class ManufacturerJdbcDao implements ManufacturerDao {
         String query = "SELECT * FROM manufacturers WHERE manufacturer_deleted = 'FALSE'";
         List<Manufacturer> manufacturers = new ArrayList<>();
         try (Connection connection = ConnectionUtil.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet != null && resultSet.next()) {
                 manufacturers.add(getManufacturer(resultSet));
@@ -75,7 +76,7 @@ public class ManufacturerJdbcDao implements ManufacturerDao {
         String query = "UPDATE manufacturers SET manufacturer_name = ?, manufacturer_country = ?"
                 + "WHERE (manufacturer_id = ?) AND (manufacturer_deleted = 'FALSE')";
         try (Connection connection = ConnectionUtil.getConnection();
-                 PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, manufacturer.getName());
             preparedStatement.setString(2, manufacturer.getCountry());
             preparedStatement.setLong(3, manufacturer.getId());
@@ -95,7 +96,7 @@ public class ManufacturerJdbcDao implements ManufacturerDao {
                 + "WHERE manufacturer_id = ?";
         int updatedRows;
         try (Connection connection = ConnectionUtil.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setLong(1, id);
             updatedRows = preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -104,17 +105,13 @@ public class ManufacturerJdbcDao implements ManufacturerDao {
         return updatedRows > 0;
     }
 
-    private Manufacturer getManufacturer(ResultSet resultSet) {
+    private Manufacturer getManufacturer(ResultSet resultSet) throws SQLException {
         Manufacturer manufacturer;
-        try {
-            Long manufacturerId = resultSet.getLong("manufacturer_id");
-            String name = resultSet.getString("manufacturer_name");
-            String country = resultSet.getString("manufacturer_country");
-            manufacturer = new Manufacturer(name, country);
-            manufacturer.setId(manufacturerId);
-        } catch (SQLException e) {
-            throw new DataProcessingException("Can't find manufacturer", e);
-        }
+        Long manufacturerId = resultSet.getObject("manufacturer_id", Long.class);
+        String name = resultSet.getObject("manufacturer_name", String.class);
+        String country = resultSet.getObject("manufacturer_country", String.class);
+        manufacturer = new Manufacturer(name, country);
+        manufacturer.setId(manufacturerId);
         return manufacturer;
     }
 }
