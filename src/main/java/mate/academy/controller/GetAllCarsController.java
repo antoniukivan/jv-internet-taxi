1ef146a1c0f257c61b695336adf251a707837c10
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-21 13:51:42 +0200

    add GetAllCarsController to controller package

---
+++ src/main/java/mate/academy/controller/GetAllCarsController.java
@@ -0,0 +1,28 @@
+package mate.academy.controller;
+
+import mate.academy.lib.Injector;
+import mate.academy.model.Car;
+import mate.academy.model.Driver;
+import mate.academy.service.CarService;
+import mate.academy.service.DriverService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class GetAllCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private final CarService carService
+            = (CarService) injector.getInstance(CarService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        List<Car> allCars = carService.getAll();
+        req.setAttribute("cars", allCars);
+        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
+    }
+}
