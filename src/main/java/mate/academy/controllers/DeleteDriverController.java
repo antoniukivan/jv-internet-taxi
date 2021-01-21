8d419bd517771feac96ef4a352eee7b3e5c7f1d3
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-21 10:54:18 +0200

    checkstyle fix 2

--- src/main/java/mate/academy/controllers/DeleteDriverController.java
+++ src/main/java/mate/academy/controllers/DeleteDriverController.java
@@ -6,12 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.academy.lib.Injector;
-import mate.academy.service.CarService;
 import mate.academy.service.DriverService;
 
 public class DeleteDriverController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate.academy");
-    private final CarService carService = (CarService) injector.getInstance(CarService.class);
     private final DriverService driverService
             = (DriverService) injector.getInstance(DriverService.class);
 
