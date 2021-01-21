180d34ab5aacb647d12f8e1a1ac09df5b4399937
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-21 14:02:59 +0200

    add DeleteCarController to controller package

--- src/main/java/mate/academy/controller/DeleteCarController.java
+++ src/main/java/mate/academy/controller/DeleteCarController.java
@@ -1,16 +1,16 @@
 package mate.academy.controller;
 
-import java.io.IOException;
-import java.util.List;
+import mate.academy.lib.Injector;
+import mate.academy.service.CarService;
+import mate.academy.service.DriverService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mate.academy.lib.Injector;
-import mate.academy.model.Car;
-import mate.academy.service.CarService;
+import java.io.IOException;
 
-public class GetAllCarsController extends HttpServlet {
+public class DeleteCarController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate.academy");
     private final CarService carService
             = (CarService) injector.getInstance(CarService.class);
@@ -18,8 +18,8 @@ public class GetAllCarsController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        List<Car> allCars = carService.getAll();
-        req.setAttribute("cars", allCars);
-        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
+        Long carId = Long.valueOf(req.getParameter("id"));
+        carService.delete(carId);
+        resp.sendRedirect(req.getContextPath() + "/cars/all");
     }
 }
